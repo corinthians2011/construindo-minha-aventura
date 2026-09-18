cd4ea1e01892ee7682fdce0466e33c73e0e425d4
corinthians2011 <adrean.matiak@escola.pr.gov.br>
2026-09-18 10:14:46 -0300

    uftuf

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ avanca.forEach(button => {
         const atual = document.querySelector('.ativo')
         const proximoPasso 'passo-' + this.getAttribute('data-proximo');
 
+        atual.classlist.remove('ativo');
         
     })
 })
